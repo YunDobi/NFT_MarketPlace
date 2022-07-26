7d842f3f75a6218cafa52e5100c83ea8c3c73995
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-07-26 12:21:29 -0700

    Views select

--- screens/Home.js
+++ screens/Home.js
@@ -1,16 +1,34 @@
-import React, {useState} from "react";
-import {View, SafeAreaView, FlatList, Text} from "react-native";
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
 
 //dummy data and images or css locations
-import {COLORS, NFTData} from "../constants";
-import { NFTcard, HomeHeader, FocusedStateBar } from "../components";
+import { COLORS, NFTData } from "../constants";
+import { NFTcard, HomeHeader, FocusedStatusBar } from "../components";
 
 function Home() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <FocusedStateBar background={COLORS.primary} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary}/>
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => {item.id}}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={HomeHeader}
+          />
+        </View>
+
+        <View style={{position:"absolute",top:0, bottom:0, right:0, left:0,zIndex:-1}}>
+          <View style={{height: 300, backgroundColor: COLORS.primary}}/>
+          <View style={{flex:1, backgroundColor: COLORS.white}}/>
+
+        </View>
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
-export default Home
+export default Home;
