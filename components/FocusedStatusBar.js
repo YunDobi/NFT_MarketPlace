9ea6e7b0d67f59baef6129ca85708dfde20d23d1
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-08-03 15:26:27 -0700

    header

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'react-native'
 import { useIsFocused} from "@react-navigation/core";
 
-const FocusedStatusBar = (props) => {
+const FocusedStatusBar = ({backgroundColor, ...props}) => {
   const isFocused = useIsFocused();
   // console.log(isFocused)
-  return isFocused ? <StatusBar animated={true} {...props}/> : null;
+  return isFocused ? <StatusBar animated={true} barStyle='light-content' backgroundColor={backgroundColor} {...props}/> : null;
 }
 
 export default FocusedStatusBar
